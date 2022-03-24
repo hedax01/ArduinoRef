3b0aa5ea3913b15b1ef94b1f76e54146a3fcf569
nqhuy <mars_in_love@hotmail.com>
2022-03-24 15:32:52 +0700

    small change

--- src/refs.ino
+++ src/refs.ino
@@ -117,3 +117,32 @@ void loop(void) {
 }
  
  */
+/**
+ * 
+ #include <Wire.h>
+#include <MAX44009.h>
+
+MAX44009 light;
+ 
+void setup() 
+{
+	Serial.begin(9600);
+	Wire.begin();
+	
+	delay(500);
+	
+	if(light.begin())
+  	{
+    		Serial.println("Could not find a valid MAX44009 sensor, check wiring!");
+		while(1);
+	}
+}
+ 
+void loop() 
+{
+	Serial.print("Light (lux):    ");
+	Serial.println(light.get_lux());
+	delay(1000);
+}
+
+*/
